6020b2452d84134fee0c25526fb52def79575a2f
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-05-29 19:38:00 -0300

    finalizando os prototipos

--- Projeto/Main.py
+++ Projeto/Main.py
@@ -1,6 +1,6 @@
 import time
 
-# Importando os arquivos do seu projeto
+#importando os arquivos do prototipo
 import MapeamentoDaOrbita
 import Lancamento
 import TrocarOrbita
@@ -8,7 +8,6 @@ import MonitoramentoDoClima
 import DesorbitagemSustentavel
 
 def exibir_menu():
-    """Função responsável por desenhar a interface do menu no terminal."""
     print("\n" + "=" * 55)
     print("SISTEMA DE GERENCIAMENTO AEROESPACIAL (GS - 1ESPY)")
     print("=" * 55)
@@ -23,19 +22,15 @@ def exibir_menu():
 
 
 def main():
-    """Função principal que gerencia o loop do programa e valida os dados."""
 
     while True:
         exibir_menu()
-
-        # Recebendo a entrada do usuário e removendo espaços em branco extras
         opcao = input("Escolha uma opção válida (0 a 6): ").strip()
 
-        # VALIDAÇÃO DE DADOS: Verifica qual opção foi escolhida
         if opcao == '1':
             print("\n>> Iniciando Descrição da Solução Proposta...")
             time.sleep(1)
-            print("\n>> DESCRIÇÃO: Sistema de monitoramento e gestão de tráfego orbital da Flyspace...")
+            print("\n>> ADICIONAR DESCRICAO AINDA !!!!")
 
         elif opcao == '2':
             print("\n>> Iniciando Módulo de Lançamento...")
@@ -59,19 +54,18 @@ def main():
 
         elif opcao == '6':
             print("\n>> Iniciando Mapeamento da Órbita...")
-            time.sleep(1)  # Pausa dramática rápida para efeito visual no terminal
+            time.sleep(1)
             MapeamentoDaOrbita.mapeamentoDaOrbita()
 
         elif opcao == '0':
             print("\n>> Encerrando o sistema da Flyspace. Até logo!\n")
-            break  # Sai do loop "while" e finaliza o programa
+            break
 
         else:
-            # Caso o usuário digite "A", "7", "teste", etc.
             print("\nERRO: Entrada inválida! Por favor, digite apenas números entre 0 e 6.")
             time.sleep(1.5)
 
 
-# Ponto de partida do programa
+#partida para executar tudo aqui
 if __name__ == "__main__":
     main()
